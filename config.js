6d25018380380487e9d12e1c71fee427609174e6
digitalsignage7129 <info@digital-led.net>
2026-09-14 16:42:36 +0900

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 window.SIGNAGE_CONFIG = {
   SUPABASE_URL: "https://mnhfiprqvwbjtkxueesu.supabase.co",
-  SUPABASE_PUBLISHABLE_KEY: "PASTE_YOUR_SB_PUBLISHABLE_KEY_HERE",
+  SUPABASE_PUBLISHABLE_KEY: "sb_publishable_Twb04ceamldk8nWOm7flKA_GiZAdkch",
   STORAGE_BUCKET: "signage-media"
 };
